7794a08aba51d13b2a55b3390772ab0367736e00
David Atchley <david.m.atchley@gmail.com>
2017-10-06 09:31:38 -0500

    fix: removed misplaced sourceMap in postcss-loader for s[ac]ss

--- packages/react-scripts/config/webpack.config.prod.js
+++ packages/react-scripts/config/webpack.config.prod.js
@@ -254,7 +254,6 @@ module.exports = {
                         ident: 'postcss',
                         plugins: () => [
                           require('postcss-flexbugs-fixes'),
-                          sourceMap: shouldUseSourceMap,
                           autoprefixer({
                             browsers: [
                               '>1%',
